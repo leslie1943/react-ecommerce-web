f8723ffbaa7ac1ce748a1c97ddcdfa6c40042814
苏震 <suzhen@viewhigh.com>
2021-01-20 13:20:32 +0800

    Singup done

--- src/components/core/SignIn.tsx
+++ src/components/core/SignIn.tsx
@@ -5,19 +5,21 @@ import Layout from './Layout'
 const SignIn = () => {
   return (
     <Layout title="登录" subTitle="飞流直下三千尺🙃">
-      <Form>
-        <Form.Item name="email" label="邮箱">
-          <Input />
-        </Form.Item>
-        <Form.Item name="password" label="密码">
-          <Input.Password />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
+      <div className="sign-form">
+        <Form>
+          <Form.Item name="email" label="邮箱">
+            <Input />
+          </Form.Item>
+          <Form.Item name="password" label="密码">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </Layout>
   )
 }
